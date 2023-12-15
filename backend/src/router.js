6ced6cc513646172bf76d43dbb7be103467fdca3
belizario87 <leonardobelizario88@gmail.com>
2023-12-15 17:42:56 -0300

    Add a rota getAll

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const tasksController = require("./controllers/tasksController");
 
 const router = express.Router();
 
-router.get("/tasks", (req, res) => {
-  res.status(200).send("Hello World");
-});
+router.get("/tasks", tasksController.getAll);
 
 module.exports = router;
