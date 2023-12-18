6ac24d05b066e2415fc6ca24ce72b418cc359688
belizario87 <leonardobelizario88@gmail.com>
2023-12-18 12:29:07 -0300

    Definindo a rota para atualizar uma tarefa

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,10 +8,15 @@ router.get("/tasks", tasksController.getAll);
 
 router.post(
   "/tasks",
-  tasksMiddlewares.validateBody,
+  tasksMiddlewares.validateFields,
   tasksController.createTask
 );
 
 router.delete("/tasks/:id", tasksController.deleteTask);
+router.put(
+  "/tasks/:id",
+  tasksMiddlewares.validateFields,
+  tasksController.updateTask
+);
 
 module.exports = router;
