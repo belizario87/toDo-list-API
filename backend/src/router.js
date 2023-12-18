fdcbb8b689d359f57bfb465dbccd3508079765aa
belizario87 <leonardobelizario88@gmail.com>
2023-12-18 11:52:47 -0300

    Inserindo a rota de deletar tarefa

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const tasksController = require("./controllers/tasksController");
+const tasksMiddlewares = require("./middlewares/tasksMiddleware");
 
 const router = express.Router();
 
 router.get("/tasks", tasksController.getAll);
 
-router.post("/tasks", tasksController.createTask);
+router.post(
+  "/tasks",
+  tasksMiddlewares.validateBody,
+  tasksController.createTask
+);
+
+router.delete("/tasks/:id", tasksController.deleteTask);
 
 module.exports = router;
